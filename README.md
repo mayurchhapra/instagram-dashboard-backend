d0f2ca2f7303300868ffb5ddd18688f2fd150d77
Mayur Chhapra <mayurchhapra@gmail.com>
2023-10-15 22:43:46 +0530

    :tada: Initial commit with Nest setup

---
+++ README.md
@@ -0,0 +1,2 @@
+## License
+# instagram-dashboard-backend
---
+++ ormcofig.js
@@ -0,0 +1,11 @@
+// ormconfig.js
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
